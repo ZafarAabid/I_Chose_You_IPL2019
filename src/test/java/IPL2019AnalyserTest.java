648db66145d8de659e5a7af5d43a2aaa0d19b349
zafaraabid <zafaraabid3295@gmail.com>
2019-12-14 16:28:34 +0530

    Add. added tests code to validate Either given data get sorted by StrikeRate or not

--- src/test/java/IPL2019AnalyserTest.java
+++ src/test/java/IPL2019AnalyserTest.java
@@ -8,11 +8,13 @@ import org.junit.Test;
 import java.util.List;
 
 /* IPL_SAMPLE_BATTING_CSV_FILE_DATA_FOR_VALIDATION
-0  ,Andre Russell       ,14 ,13  ,4 ,510 ,80* ,56.66,249,204.81    ,0  ,4 ,31,52
-1  ,Virat Kohli         ,14 ,14  ,0 ,464 ,100 ,33.14,328,141.46    ,1  ,2 ,46,13
-2 ,AB de Villiers      ,13 ,13  ,3 ,442 ,82* ,44.2 ,287,154       ,0  ,5 ,31,26
-3 ,MS Dhoni            ,15 ,12  ,7 ,416 ,84* ,83.2 ,309,134.62    ,2  ,3 ,22,23
-4 ,Moeen Ali           ,11 ,10  ,2 ,220 ,66  ,27.5 ,133,165.41    ,2  ,2 ,16,17*/
+POS,PLAYER              ,Mat  ,Inns  ,NO  ,Runs  ,HS   ,Avg    ,BF            ,StrikeRate  ,100  ,50  ,4s  ,6s
+0  ,Andre Russell       ,14   ,13    ,4   ,510   ,80*  ,56.66  ,249           ,204.81      ,0    ,4   ,31  ,52
+1  ,Virat Kohli         ,14   ,14    ,0   ,464   ,100  ,33.14  ,328           ,141.46      ,1    ,2   ,46  ,13
+2 ,AB de Villiers       ,13   ,13    ,3   ,442   ,82*  ,44.2   ,287           ,154         ,0    ,5   ,31  ,26
+3 ,MS Dhoni             ,15   ,12    ,7   ,416   ,84*  ,83.2   ,309           ,134.62      ,2    ,3   ,22  ,23
+4 ,Moeen Ali            ,11   ,10    ,2   ,220   ,66   ,27.5   ,133           ,165.41      ,2    ,2   ,16  ,17
+*/
 
 public class IPL2019AnalyserTest {
     public static final String IPL_BATTING_DATA_CSV_FILE = "/home/user/workspace/IchooseYouIPL/src/test/resources/IPL2019FactsheetMostRuns.csv";
@@ -94,7 +96,9 @@ public class IPL2019AnalyserTest {
         IPLAnalyser iplAnalyzer = new IPLAnalyser();
         try {
             List<IplBatsmanData> iplDataList = iplAnalyzer.loadBattingData(IplBatsmanData.class, IPL_SAMPLE_BATTING_DATA_CSV_FILE);
+
             iplDataList = iplAnalyzer.sortByParamter(ComparatorParameters.Parameter.AVERAGE,iplDataList);
+
             Assert.assertEquals("Moeen Ali", (iplDataList.get(0).playerName).trim());
             Assert.assertEquals("Virat Kohli", (iplDataList.get(1).playerName).trim());
             Assert.assertEquals("AB de Villiers", (iplDataList.get(2).playerName).trim());
@@ -104,5 +108,36 @@ public class IPL2019AnalyserTest {
             Assert.assertEquals(e.type, IPLAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
     }
+
+    @Test
+    public void forGivenCsv_WhenFetchTheData_IfSortedByStrikeRatefullReturnTrue() {
+        IPLAnalyser iplAnalyzer = new IPLAnalyser();
+        try {
+            List<IplBatsmanData> iplDataList = iplAnalyzer.loadBattingData(IplBatsmanData.class, IPL_BATTING_DATA_CSV_FILE);
+            iplDataList = iplAnalyzer.sortByParamter(ComparatorParameters.Parameter.STRIKE_RATE,iplDataList);
+            Assert.assertEquals("Harpreet Brar", (iplDataList.get(0).playerName).trim());
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(e.type, IPLAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+    }
+
+    @Test
+    public void forGivenSampleCsv_WhenFetchTheData_IfSortedByStrikeRatefullReturnTrue() {
+        IPLAnalyser iplAnalyzer = new IPLAnalyser();
+        try {
+            List<IplBatsmanData> iplDataList = iplAnalyzer.loadBattingData(IplBatsmanData.class, IPL_SAMPLE_BATTING_DATA_CSV_FILE);
+
+            iplDataList = iplAnalyzer.sortByParamter(ComparatorParameters.Parameter.STRIKE_RATE,iplDataList);
+
+            Assert.assertEquals("MS Dhoni", (iplDataList.get(0).playerName).trim());
+            Assert.assertEquals("Virat Kohli", (iplDataList.get(1).playerName).trim());
+            Assert.assertEquals("AB de Villiers", (iplDataList.get(2).playerName).trim());
+            Assert.assertEquals("Moeen Ali", (iplDataList.get(3).playerName).trim());
+            Assert.assertEquals("Andre Russell", (iplDataList.get(4).playerName).trim());
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(e.type, IPLAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+    }
+
 
 }
