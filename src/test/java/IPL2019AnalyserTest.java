2e8d8c24210b964e240b98381a5907e5c83a1255
zafaraabid <zafaraabid3295@gmail.com>
2019-12-14 12:21:15 +0530

    Add.simple test to check either data is being pop-up or not while reading csv file

--- src/test/java/IPL2019AnalyserTest.java
+++ src/test/java/IPL2019AnalyserTest.java
@@ -1,3 +1,24 @@
-public class IPL2019AnalyserTest {
+import com.google.gson.Gson;
+import com.ipl.CensusAnalyserException;
+import com.ipl.IPLAnalyser;
+import com.ipl.IplBatsmanData;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
 
+public class IPL2019AnalyserTest {
+    public static final String IPL_BATTING_DATA_CSV_FILE = "/home/user/workspace/IchooseYouIPL/src/test/resources/IPL2019FactsheetMostRuns.csv";
+    public static final String IPL_BOWLING_DATA_CSV_FILE = "/home/user/workspace/IchooseYouIPL/src/test/resources/IPL2019FactsheetMostWkts.csv";
+    @Test
+    public void name() {
+        IPLAnalyser iplAnalyzer = new IPLAnalyser();
+        try {
+            List<IplBatsmanData> iplDataList = iplAnalyzer.loadBattingData(IplBatsmanData.class,IPL_BATTING_DATA_CSV_FILE);
+            iplDataList.forEach(System.out::println);
+            Assert.assertEquals("David Warner",(iplDataList.get(0).PlayerName).trim());
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
